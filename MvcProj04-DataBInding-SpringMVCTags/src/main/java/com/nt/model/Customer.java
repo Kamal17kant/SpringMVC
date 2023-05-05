7f1a0307656b16299064519b02a72d01a9aa9463
KAMAL <KAMAL@gmail.com>
2023-05-05 11:32:35 +0530

    update

---
+++ MvcProj04-DataBInding-SpringMVCTags/src/main/java/com/nt/model/Customer.java
@@ -0,0 +1,35 @@
+package com.nt.model;
+
+import lombok.Data;
+
+@Data 
+public class Customer {
+	private int cNo=1234;
+	public int getcNo() {
+		return cNo;
+	}
+	public void setcNo(int cNo) {
+		this.cNo = cNo;
+	}
+	public String getcName() {
+		return cName;
+	}
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+	public String getcAddress() {
+		return cAddress;
+	}
+	public void setcAddress(String cAddress) {
+		this.cAddress = cAddress;
+	}
+	public double getBillAmt() {
+		return billAmt;
+	}
+	public void setBillAmt(double billAmt) {
+		this.billAmt = billAmt;
+	}
+	private String cName="Raja";
+	private String cAddress="gaya";
+	private double billAmt;
+}
